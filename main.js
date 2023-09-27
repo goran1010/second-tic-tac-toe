a82503ebbf225cfc737e4e40dcba9d01d0630701
Goran Jovic <goran1010jovic@gmail.com>
2023-09-27 12:57:27 +0200

    Need to do AI

--- main.js
+++ main.js
@@ -3,15 +3,7 @@ const gameBoard = {
   currentPlayer: {},
 };
 
-class Player {
-  constructor(name, sign) {
-    this.name = name;
-    this.sign = sign;
-  }
-}
-
-const playerX = new Player("Player X", "X");
-const playerO = new Player("Player O", "O");
+let gameOver = false;
 
 const allElements = Array.from(document.querySelectorAll(".element"));
 allElements.forEach((element) => element.addEventListener(`click`, playSign));
@@ -20,6 +12,28 @@ const newTwoPlayerButton = document.querySelector(`.new-game`);
 newTwoPlayerButton.addEventListener(`click`, newGame);
 
 const announcements = document.querySelector(".announcements");
+
+const resetWins = document.querySelector(`.reset-wins`);
+resetWins.addEventListener(`click`, () => {
+  playerX.wins = 0;
+  playerO.wins = 0;
+  playerXWins.textContent = 0;
+  playerOWins.textContent = 0;
+});
+
+let playerXWins = document.querySelector(`#player-x-wins`);
+let playerOWins = document.querySelector(`#player-o-wins`);
+
+class Player {
+  constructor(name, sign, wins) {
+    this.name = name;
+    this.sign = sign;
+    this.wins = wins;
+  }
+}
+
+const playerX = new Player("Player X", "X", +playerXWins.textContent);
+const playerO = new Player("Player O", "O", +playerOWins.textContent);
 
 let playerXName = document.querySelector(`#player-x-input`);
 playerXName.addEventListener(`keyup`, () => {
@@ -39,7 +53,17 @@ const swapPlayers = document.querySelector(`.swap-players`);
 swapPlayers.addEventListener(`click`, () => {
   newGame();
 
-  if (playerX.name === "Player X" && playerO.name === "Player O") return;
+  if (playerX.name === "Player X" && playerO.name === "Player O") {
+    let tempName = playerXWins.textContent;
+    playerXWins.textContent = playerOWins.textContent;
+    playerOWins.textContent = tempName;
+
+    tempName = playerX.wins;
+    playerX.wins = playerO.wins;
+    playerO.wins = tempName;
+
+    return;
+  }
 
   if (playerX.name === "Player X") {
     playerX.name = playerO.name;
@@ -47,6 +71,14 @@ swapPlayers.addEventListener(`click`, () => {
 
     playerO.name = "Player O";
     playerOName.value = "";
+
+    let tempName = playerX.wins;
+    playerX.wins = playerO.wins;
+    playerO.wins = tempName;
+
+    tempName = playerXWins.textContent;
+    playerXWins.textContent = playerOWins.textContent;
+    playerOWins.textContent = tempName;
 
     updateDisplay();
     return;
@@ -57,6 +89,14 @@ swapPlayers.addEventListener(`click`, () => {
 
     playerX.name = "Player X";
     playerXName.value = "";
+
+    let tempName = playerX.wins;
+    playerX.wins = playerO.wins;
+    playerO.wins = tempName;
+
+    tempName = playerXWins.textContent;
+    playerXWins.textContent = playerOWins.textContent;
+    playerOWins.textContent = tempName;
 
     updateDisplay();
     return;
@@ -69,6 +109,14 @@ swapPlayers.addEventListener(`click`, () => {
   tempName = playerXName.value;
   playerXName.value = playerOName.value;
   playerOName.value = tempName;
+
+  tempName = playerX.wins;
+  playerX.wins = playerO.wins;
+  playerO.wins = tempName;
+
+  tempName = playerXWins.textContent;
+  playerXWins.textContent = playerOWins.textContent;
+  playerOWins.textContent = tempName;
   updateDisplay();
 });
 
@@ -80,32 +128,132 @@ function nextPlayer() {
 }
 
 function checkGameWon() {
-  return false;
+  if (
+    gameBoard.board[0] != "" &&
+    gameBoard.board[0] === gameBoard.board[1] &&
+    gameBoard.board[1] === gameBoard.board[2]
+  ) {
+    gameWon();
+    gameOver = true;
+    allElements[0].classList.add(`win`);
+    allElements[1].classList.add(`win`);
+    allElements[2].classList.add(`win`);
+  }
+  if (
+    gameBoard.board[3] != "" &&
+    gameBoard.board[3] === gameBoard.board[4] &&
+    gameBoard.board[4] === gameBoard.board[5]
+  ) {
+    gameWon();
+    gameOver = true;
+    allElements[3].classList.add(`win`);
+    allElements[4].classList.add(`win`);
+    allElements[5].classList.add(`win`);
+  }
+  if (
+    gameBoard.board[6] != "" &&
+    gameBoard.board[6] === gameBoard.board[7] &&
+    gameBoard.board[7] === gameBoard.board[8]
+  ) {
+    gameWon();
+    gameOver = true;
+    allElements[6].classList.add(`win`);
+    allElements[7].classList.add(`win`);
+    allElements[8].classList.add(`win`);
+  }
+  if (
+    gameBoard.board[0] != "" &&
+    gameBoard.board[0] === gameBoard.board[3] &&
+    gameBoard.board[3] === gameBoard.board[6]
+  ) {
+    gameWon();
+    gameOver = true;
+    allElements[0].classList.add(`win`);
+    allElements[3].classList.add(`win`);
+    allElements[6].classList.add(`win`);
+  }
+  if (
+    gameBoard.board[1] != "" &&
+    gameBoard.board[1] === gameBoard.board[4] &&
+    gameBoard.board[4] === gameBoard.board[7]
+  ) {
+    gameWon();
+    gameOver = true;
+    allElements[1].classList.add(`win`);
+    allElements[4].classList.add(`win`);
+    allElements[7].classList.add(`win`);
+  }
+  if (
+    gameBoard.board[2] != "" &&
+    gameBoard.board[2] === gameBoard.board[5] &&
+    gameBoard.board[5] === gameBoard.board[8]
+  ) {
+    gameWon();
+    gameOver = true;
+    allElements[2].classList.add(`win`);
+    allElements[5].classList.add(`win`);
+    allElements[8].classList.add(`win`);
+  }
+  if (
+    gameBoard.board[0] != "" &&
+    gameBoard.board[0] === gameBoard.board[4] &&
+    gameBoard.board[4] === gameBoard.board[8]
+  ) {
+    gameWon();
+    gameOver = true;
+    allElements[0].classList.add(`win`);
+    allElements[4].classList.add(`win`);
+    allElements[8].classList.add(`win`);
+  }
+  if (
+    gameBoard.board[2] != "" &&
+    gameBoard.board[2] === gameBoard.board[4] &&
+    gameBoard.board[4] === gameBoard.board[6]
+  ) {
+    gameWon();
+    gameOver = true;
+    allElements[2].classList.add(`win`);
+    allElements[4].classList.add(`win`);
+    allElements[6].classList.add(`win`);
+  }
+}
+
+function gameWon() {
+  gameBoard.currentPlayer.wins++;
+  announcements.textContent = `${gameBoard.currentPlayer.name} has WON !!!`;
+  playerXWins.textContent = playerX.wins;
+  playerOWins.textContent = playerO.wins;
+
+  gameBoard.board.forEach((board, index) => {
+    allElements[index].textContent = board;
+  });
+}
+
+function gameDraw() {
+  announcements.textContent = `Game is a DRAW`;
+  gameBoard.board.forEach((board, index) => {
+    allElements[index].textContent = board;
+  });
 }
 
 function checkGameDraw() {
   for (let i = 0; i < gameBoard.board.length; i++) {
     if (gameBoard.board[i] === "") return false;
   }
-  return true;
+  gameDraw();
+  gameOver = true;
 }
 
 function updateDisplay() {
-  if (checkGameDraw()) {
-    announcements.textContent = `Game is a DRAW`;
-    gameBoard.board.forEach((board, index) => {
-      allElements[index].textContent = board;
-    });
-    return;
-  }
-  announcements.textContent = `${gameBoard.currentPlayer.name}'s turn to play`;
+  if (gameOver) return;
+  announcements.textContent = `${gameBoard.currentPlayer.name} PLAYING`;
   gameBoard.board.forEach((board, index) => {
     allElements[index].textContent = board;
   });
 }
 
 function playSign() {
-  if (checkGameWon() || checkGameDraw()) {
+  if (gameOver) {
     return;
   }
   if (this.textContent) return;
@@ -115,11 +263,18 @@ function playSign() {
   checkGameWon();
   checkGameDraw();
 
+  if (gameOver) return;
   gameBoard.currentPlayer = nextPlayer();
   updateDisplay();
 }
 
 function newGame() {
+  gameOver = false;
+
+  allElements.forEach((element) => {
+    element.classList.remove(`win`);
+  });
+
   gameBoard.board = ["", "", "", "", "", "", "", "", ""];
   gameBoard.currentPlayer = playerX;
 
